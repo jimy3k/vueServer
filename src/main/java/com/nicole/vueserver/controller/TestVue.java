f1b913ae736b43d36cdab673c03a9a85dde0e2a8
jimy3k <jimy3k@126.com>
2022-09-19 17:56:40 +0800

    新增各种组件

--- src/main/java/com/nicole/vueserver/controller/TestVue.java
+++ src/main/java/com/nicole/vueserver/controller/TestVue.java
@@ -1,10 +1,13 @@
 package com.nicole.vueserver.controller;
 
+import com.nicole.vueserver.entity.User;
 import com.nicole.vueserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,8 +21,8 @@ public class TestVue {
         return "你小子测试成功！";
     }
 
-    @GetMapping("/getuser")
-    public String getuser(){
-        return "你小子测试成功！";
+    @GetMapping("/getuserlist")
+    public List<User> getuserlist(){
+        return userService.list();
     }
 }
